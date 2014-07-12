888508cb4869420c9fbbf94dbda3e6f60c9ad635
Giulia Vergottini <hello@railslide.io>
2014-07-13 00:00:37 +0200

    change admin field name for polls

--- polls/models.py
+++ polls/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 # Create your models here.
 class Poll(models.Model):
-    question = models.CharField(max_length=200)
+    question = models.CharField('Poll', max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
@@ -13,6 +13,10 @@ class Poll(models.Model):
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
